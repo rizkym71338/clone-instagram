d461eaa73657e9409a6c1e100beed0fd0a9ea716
rizkym71338 <rizkym71338@gmail.com>
2021-12-24 13:37:43 +0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Beranda from "./pages/Beranda"
-import Login from "./pages/Login/Index.jsx"
+import Login from "./pages/Login/Index"
 import Chat from "./pages/Chat"
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
 
-function App() {
+const App = () => {
   return (
     <Routes>
       <Route path="/*" element={<Beranda />} />
-      <Route path="login" element={<Login />} />
-      <Route path="chat" element={<Chat />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/chat" element={<Chat />} />
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
